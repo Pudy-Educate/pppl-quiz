94ba4d624e6c88d30d0ea66d9b763b6a7e8ed857
pudyasta <pudyastasatria@gmail.com>
2024-05-16 09:04:00 +0700

    feat(test)new method

--- src/test/java/org/example/AutoTest.java
+++ src/test/java/org/example/AutoTest.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import dev.failsafe.internal.util.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class AutoTest {
@@ -13,4 +17,26 @@ public class AutoTest {
             Thread.sleep(10000);
             driver.quit();
         }
+
+
+    @Test
+    public void testLogin() throws InterruptedException {
+        System.setProperty("webdriver.gecko.driver", "D:\\KULIAH\\Sem4\\Pengujian\\KUIS\\kuis1\\drivers\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.saucedemo.com/");
+        WebElement uname = driver.findElement(By.id("user-name"));
+        WebElement pass = driver.findElement(By.id("password"));
+        WebElement btn = driver.findElement(By.id("login-button"));
+        uname.sendKeys("standard_user");
+        pass.sendKeys("secret_sauce");
+        btn.submit();
+
+        String expectedUrl = "https://www.saucedemo.com/inventory.html";
+        String actualUrl = driver.getCurrentUrl();
+        WebElement text = driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/span"));
+        text.isDisplayed();
+        Assertions.assertEquals(actualUrl, expectedUrl,"Test success");
+
+        driver.quit();
+    }
 }
